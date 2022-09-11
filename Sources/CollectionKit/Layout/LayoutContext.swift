767fac2efe63417fbc9871291d82e7fb5626cb6b
Dmitry <d@kozlov.ru>
2022-09-11 15:49:43 +0300

    Added proxy layout context

--- Sources/CollectionKit/Layout/LayoutContext.swift
+++ Sources/CollectionKit/Layout/LayoutContext.swift
@@ -15,3 +15,15 @@ public protocol LayoutContext {
   func identifier(at: Int) -> String
   func size(at index: Int, collectionSize: CGSize) -> CGSize
 }
+public protocol ProxyLayoutContext: LayoutContext {
+  var context: LayoutContext { get }
+}
+extension ProxyLayoutContext {
+  public var collectionSize: CGSize { context.collectionSize }
+  public var numberOfItems: Int { context.numberOfItems }
+  public func data(at: Int) -> Any { context.data(at: at) }
+  public func identifier(at: Int) -> String { context.identifier(at: at) }
+  public func size(at index: Int, collectionSize: CGSize) -> CGSize {
+    context.size(at: index, collectionSize: collectionSize)
+  }
+}
