8f8e5bfb03eff40b94cabe1698e7bbdbfba04f67
Dmitry <d@kozlov.ru>
2022-09-11 15:50:51 +0300

    Refactored inset layout

--- Sources/CollectionKit/Layout/InsetLayout.swift
+++ Sources/CollectionKit/Layout/InsetLayout.swift
@@ -9,57 +9,43 @@
 import UIKit
 
 open class InsetLayout: WrapperLayout {
+  public var rootLayout: Layout
   public var insets: UIEdgeInsets
   public var insetProvider: ((CGSize) -> UIEdgeInsets)?
-
-  struct InsetLayoutContext: LayoutContext {
-    var original: LayoutContext
-    var insets: UIEdgeInsets
-
-    var collectionSize: CGSize {
-      return original.collectionSize.insets(by: insets)
-    }
-    var numberOfItems: Int {
-      return original.numberOfItems
-    }
-    func data(at: Int) -> Any {
-      return original.data(at: at)
-    }
-    func identifier(at: Int) -> String {
-      return original.identifier(at: at)
-    }
-    func size(at: Int, collectionSize: CGSize) -> CGSize {
-      return original.size(at: at, collectionSize: collectionSize)
-    }
-  }
-
   public init(_ rootLayout: Layout, insets: UIEdgeInsets = .zero) {
     self.insets = insets
-    super.init(rootLayout)
+    self.rootLayout = rootLayout
   }
 
   public init(_ rootLayout: Layout, insetProvider: @escaping ((CGSize) -> UIEdgeInsets)) {
     self.insets = .zero
     self.insetProvider = insetProvider
-    super.init(rootLayout)
+    self.rootLayout = rootLayout
   }
 
-  open override var contentSize: CGSize {
-    return rootLayout.contentSize.insets(by: -insets)
+  open var contentSize: CGSize {
+    rootLayout.contentSize.insets(by: -insets)
   }
 
-  open override func layout(context: LayoutContext) {
+  open func layout(context: LayoutContext) {
     if let insetProvider = insetProvider {
       insets = insetProvider(context.collectionSize)
     }
-    rootLayout.layout(context: InsetLayoutContext(original: context, insets: insets))
+    rootLayout.layout(context: Context(context: context, insets: insets))
   }
 
-  open override func visibleIndexes(visibleFrame: CGRect) -> [Int] {
-    return rootLayout.visibleIndexes(visibleFrame: visibleFrame.inset(by: -insets))
+  open func visibleIndexes(visibleFrame: CGRect) -> [Int] {
+    rootLayout.visibleIndexes(visibleFrame: visibleFrame.inset(by: -insets))
   }
-
-  open override func frame(at: Int) -> CGRect {
-    return rootLayout.frame(at: at) + CGPoint(x: insets.left, y: insets.top)
+  open func frame(at: Int) -> CGRect {
+    rootLayout.frame(at: at) + CGPoint(x: insets.left, y: insets.top)
+  }
+  
+  private struct Context: ProxyLayoutContext {
+    var context: LayoutContext
+    var insets: UIEdgeInsets
+    var collectionSize: CGSize {
+      context.collectionSize.insets(by: insets)
+    }
   }
 }
